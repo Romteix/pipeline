f95d6d2894b5b959af3c19190acda93d6b682bc2
Romteix <git.unsocial052@passinbox.com>
2024-10-07 14:05:00 +0200

    Hello World feature

---
+++ src/test/java/fr/m2i/api/controllers/HelloControllerTest.java
@@ -0,0 +1,30 @@
+package fr.m2i.api.controllers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testGetHello() throws Exception {
+        MvcResult result = mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String resultStr = result.getResponse().getContentAsString();
+        assertEquals("Hello World", resultStr);
+    }
+}
